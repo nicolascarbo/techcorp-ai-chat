8bad1b691226c8f8b78222765f259442df88a066
RobinK <robin.kilbertus@gmail.com>
2026-06-30 11:20:45 +0200

    feat : ollama service

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,13 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      '/ollama': {
+        target: 'http://localhost:11434',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ollama/, ''),
+      },
+    },
+  },
 });
